ca82381eeb6f346042710ab9eea9b4f3d762d67a
leanhtu188 <leanhtu188@gmail.com>
2016-12-31 13:12:32 +0800

    Library Controller methods, forgot to push last time

--- Epitrain_5.2_v2/app/Http/Controllers/librarycontroller.php
+++ Epitrain_5.2_v2/app/Http/Controllers/librarycontroller.php
@@ -3,84 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use Auth;
+use Carbon\Carbon;
 use App\Http\Requests;
-use App\Book;
+use App\Http\Controllers\Controller;
 
-class librarycontroller extends Controller
+class LibraryController extends Controller
 {
-   /**
-   * Display a listing of the resource.
-   *
-   * @return Response
-   */
-  public function index()
-  {
-    $books = Book::all();
-		//$books->toarray();
-    return \View::make('books.index', compact('books'));
-  }
-
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return Response
-   */
-  public function create()
-  {
     //
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @return Response
-   */
-  public function store()
-  {
-    //
-  }
-
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function show($id)
-  {
-    //
-  }
-
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function edit($id)
-  {
-    //
-  }
-
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function update($id)
-  {
-    //
-  }
-
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function destroy($id)
-  {
-    //
-  }
+		/**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+		
+		public function buy($book_id)
+		{
+				$user_id = Auth::user()->id;
+				$current_time = Carbon::now()->toDayDateTimeString();
+				$id = DB::table('libraries')->insertGetId(
+						['user_id' => $user_id, 'book_id' => $book_id, 'created_at' => $current_time]
+				);
+				
+				//echo $id;
+				return redirect('mylibrary');
+		}
 }
