34a53bb1afcb8390164387f1da046e23819b1c7c
leanhtu188 <leanhtu188@gmail.com>
2017-04-09 04:08:04 +0800

    Changed user create password creation, added email noti

---
+++ Epitrain_5.2_v2/resources/views/emails/UserCreated.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<style>
+
+</style>
+	
+		<h1> Hello, <?php echo $name;?><h1>
+	<body style="font-size:12px">
+		<p>Welcome to Epitrain!
+		<br>
+		<br>
+		An account has been created for you with the following credentials:
+		<br>
+		Username: <?php echo $username;?>
+		<br>
+		Password: <?php echo $password;?>
+		<br>
+		<br>
+		For your security, please do change your password when you log in!<br>
+		Again, welcome to Epitrain! We hope you will have an enjoyable experience!</p>
+	</body>
+</html>
