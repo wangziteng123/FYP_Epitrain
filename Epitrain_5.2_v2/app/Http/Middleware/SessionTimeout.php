fe10ae7c89da077cf4c7d4c7748ee6e0c1f2f76d
asril.lee.2014 <asril.lee.2014@smu.edu.sg>
2017-04-16 20:50:48 +0800

    Fixed SessionTimeout

--- Epitrain_5.2_v2/app/Http/Middleware/SessionTimeout.php
+++ Epitrain_5.2_v2/app/Http/Middleware/SessionTimeout.php
@@ -32,12 +32,12 @@ class SessionTimeout {
             if ($user_record != null) { 
             	// if user's last activity was at least $sessionTime->session_time minutes ago, logs him/her out
                 if (time() - $user_record->last_activity > $sessionTime->session_time) { 
-                    auth()->logout();
-                    $sTime = $sessionTime->session_time/60;
                     DB::table('sessions')->where('user_id', auth()->user()->id)
                     ->update(
                         ['loggedIn' => 0]
                     );
+                    auth()->logout();
+                    $sTime = $sessionTime->session_time/60;
                     return redirect('login')->with('message', 'You were inactive for more than '.$sTime.' minutes. This platform will automatically log you out after '.$sTime.' minutes.');
                 } else {
                 // if not, update user's latest activity
