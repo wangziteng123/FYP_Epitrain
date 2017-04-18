106b3fa4503d75bba5dc7d4a2235a12da7d891cf
EstherGao <shuang.gao.2014@sis.smu.edu.sg>
2017-04-18 15:40:15 +0800

    documentation for the first five controller

--- Epitrain_5.2_v2/app/Http/Controllers/AdminController.php
+++ Epitrain_5.2_v2/app/Http/Controllers/AdminController.php
@@ -7,15 +7,29 @@ use DB;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+/**
+ * AdminController Class used for Contact Admin function
+ */
 class AdminController extends Controller
 {
-    //
-     public function index()
+    /**
+	 * index
+	 *
+	 * generate the admin view page
+	 *
+	 * @return view of the admin page
+	 */
+	public function index()
     {
-        //
         return \View::make('admin.admin');
     }
     
+	/**
+	 * changeEmail function change the email address linked to admin account
+	 *
+	 * @param Request $request which is the request to change admin email address
+	 * @return view of success or error message
+	 */
     public function changeEmail(Request $request){
         $changeAdminEmail = $request->get('adminEmail');
         $error = "";
@@ -45,6 +59,12 @@ class AdminController extends Controller
         }
     }
     
+	/**
+	 * changeSessionTimeout function change the setting of the time for auto timeout 
+	 *
+	 * @param Request $request which is the request to edit session time out
+	 * @return view of success or error message
+	 */
     public function changeSessionTimeout(Request $request){
         $timing = $request->get('sessionTime');
         $checkTiming = ctype_digit($timing);
--- Epitrain_5.2_v2/app/Http/Controllers/FaqController.php
+++ Epitrain_5.2_v2/app/Http/Controllers/FaqController.php
@@ -7,15 +7,28 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+/**
+ * AdminController Class used for FAQ function
+ */
 class FaqController extends Controller
 {
-    //
+    
+	/**
+	 * index function generate the FAQ page
+	 *
+	 * @return view of the FAQ page
+	 */
      public function index()
     {
-        //
         return \View::make('faq.faq');
     }
 
+	/**
+	 * create function create a new faq with question and answer
+	 *
+	 * @param Request $request takes in the question and answer content entered
+	 * @return void
+	 */
     public function create(Request $request)
     {
         $question = $request->input('question');
@@ -28,6 +41,12 @@ class FaqController extends Controller
         return redirect()->route('faq');
     }
 
+	/**
+	 * delete function delete a faq with question and answer
+	 *
+	 * @param Request $request takes in the question and answer of an existing FAQ
+	 * @return void
+	 */
     public function delete(Request $request){
         $id = $request->get('id');
         
@@ -37,6 +56,12 @@ class FaqController extends Controller
         return redirect()->route('faq');  
     }
 
+	/**
+	 * edit function edit the content of a faq
+	 *
+	 * @param Request $request takes in the question and answer content entered
+	 * @return void
+	 */
     public function edit(Request $request) {
         $id = $request->get('id');
         $question = $request->input('question');
